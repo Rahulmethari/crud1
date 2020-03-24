7e738aeb77f2f557b3d93399bd7e094edb453655
Rahulmethari <33793049+Rahulmethari@users.noreply.github.com>
2020-03-24 13:25:09 +0000

    Update database.php

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 // used to connect to the database
-$host = "localhost";
+$host = "us-cdbr-iron-east-01.cleardb.net";
 $db_name = "crud1";
-$username = "root";
-$password = "";
+$username = "b58177eca4226d";
+$password = "ba58af97";
 
 try {
     $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
