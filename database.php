642b61ba1d3a13ac1bb6ae9d8a3334a3bcba8133
Rahulmethari <33793049+Rahulmethari@users.noreply.github.com>
2020-03-24 11:22:10 +0000

    Update database.php

--- database.php
+++ database.php
@@ -1,12 +1,14 @@
 <?php
 // used to connect to the database
-$host = "q68u8b2buodpme2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$db_name = "crud1";
-$username = "x5japq6t7xrd4bz6";
-$password = "e9h4qzt64hd5bzat";
+
+class Database
+$dbservername = "q68u8b2buodpme2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dbname = "crud1";
+$dbusername = "x5japq6t7xrd4bz6";
+$dbpassword = "e9h4qzt64hd5bzat";
 
 try {
-    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+    $con = new PDO("mysql:host={$dbservername};dbname={$dbname}", $dbusername, $dbpassword);
 }
 
 // show error
