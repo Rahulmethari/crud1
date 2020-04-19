69906fc5d933fbea7576eb28505e3657e144fa8d
Rahulmethari <33793049+Rahulmethari@users.noreply.github.com>
2020-04-19 20:46:02 +0100

    Update database.php

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 // used to connect to the database
 $host = "us-cdbr-iron-east-01.cleardb.net";
-$db_name = "heroku_81912b039ccbfc5";
-$username = "b58177eca4226d";
-$password = "ba58af97";
+$db_name = "heroku_f1775403e255be";
+$username = "b6fc99c0d984fb";
+$password = "125b4491";
 
 try {
     $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
